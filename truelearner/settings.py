5c9454ccf021c8aff6dd8061088fd103c993aa3f
anmamun0 <anmamun0@gmail.com>
2025-02-01 02:56:16 +0600

    onrender csrf trusted origins error fixed

--- truelearner/settings.py
+++ truelearner/settings.py
@@ -37,7 +37,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 # CORS_ALLOWED_ORIGINS = [
 #     "http://127.0.0.1:5501",  # Frontend URL
 # ]
-CSRF_TRUSTED_ORIGINS = ['https://truelearner-backends.onrender.com/','https://*.127.0.0.1','http://127.0.0.1:5501',]
+CSRF_TRUSTED_ORIGINS = ['https://truelearner-backends.onrender.com','https://*.127.0.0.1','http://127.0.0.1:5501',]
 
  
  
