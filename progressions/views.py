5254d144d4fc0d22deae64644aa85422ae9109da
anmamun0 <anmamun0@gmail.com>
2025-03-27 01:26:03 +0600

    update_progress error fix put to post

--- progressions/views.py
+++ progressions/views.py
@@ -18,7 +18,7 @@ class ProgressViewSerializers(ModelViewSet):
     filter_backends = (rest_framework.DjangoFilterBackend,)
     filterset_class = ProgressFilter
     
-    @action(detail=True,methods=['put'],url_path='update')
+    @action(detail=True,methods=['post'],url_path='update')
     def update_progress(self,request,pk=None):
         progres = Progres.objects.get(pk=pk)
         video_id = request.data.get('video_id')
