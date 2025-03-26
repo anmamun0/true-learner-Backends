ec02d0af870454fd986dfbc3ab65d27a5d62944c
anmamun0 <anmamun0@gmail.com>
2025-03-27 00:12:41 +0600

    progress add total video in a single course

--- progressions/serializers.py
+++ progressions/serializers.py
@@ -2,7 +2,11 @@ from .models import Progres
 from rest_framework import serializers
 
 class ProgressSerializers(serializers.ModelSerializer):
+    total_video = serializers.SerializerMethodField()
+
     class Meta:
         model = Progres
-        fields = "__all__"
-        
+        fields = ['id','student','course','video_list','total_video']
+
+    def get_total_video(self,obj):
+        return obj.course.videos.count()
